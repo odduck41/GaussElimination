4f31707bb5dde5b68a1e8bcfc11f3643bd711e59
leo <gd.cats.creators@gmail.com>
2026-03-02 09:29:40 +0300

    bug fixes

--- main.c
+++ main.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-struct Matrix {
+typedef struct {
     long long **matrix;
-    size_t size = 0;
-}
+    size_t size;
+} Matrix;
 
-Matrix* create(size_t sz = 0) {
-    Matrix* m = malloc(sizeof(Matrix));
-    m->matrix = malloc(sz * sizeof(long long*));
+Matrix* create(size_t sz) {
+    Matrix* m = (Matrix*)malloc(sizeof(Matrix));
+    m->matrix = (long long**)malloc(sz * sizeof(long long*));
     for (size_t i = 0; i < sz; ++i) {
-        (m->matrix)[i] = malloc(sz * sizeof(long long));
-	(m->matrix)[i][j] = 0;
+        (m->matrix)[i] = (long long*)malloc(sz * sizeof(long long));
+	for (size_t j = 0; j < sz; ++j) (m->matrix)[i][j] = 0;
     }
     return m;
 }
