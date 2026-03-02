ca61f83fd03d4b15a5bc0ee0a187ec92cf7a9236
leo <gd.cats.creators@gmail.com>
2026-03-02 09:25:31 +0300

    simple matrix struct

--- main.c
+++ main.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
 
+struct Matrix {
+    long long **matrix;
+    size_t size = 0;
+}
+
+Matrix* create(size_t sz = 0) {
+    Matrix* m = malloc(sizeof(Matrix));
+    m->matrix = malloc(sz * sizeof(long long*));
+    for (size_t i = 0; i < sz; ++i) {
+        (m->matrix)[i] = malloc(sz * sizeof(long long));
+	(m->matrix)[i][j] = 0;
+    }
+    return m;
+}
+
+void destroy(Matrix** mat) {
+    auto m = *mat;
+    for (size_t i = 0; i < m->size; ++i) {
+        free((m->matrix)[i]);
+    }
+    free(m->matrix);
+    free(m);
+    *mat = nullptr;
+}
+
+
 int main() {
-    int a, b;
-    scanf("%d%d", &a, &b);
-    printf("a + b = %d", a + b);
+    
 }
