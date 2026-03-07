01bf68e4ce9e11907dfbb2314a9bdea03dc5d2cd
leo <gd.cats.creators@gmail.com>
2026-03-08 01:24:36 +0300

    ignore .venv

---
+++ Test.c
@@ -0,0 +1,3 @@
+//
+// Created by Lev Dymchenko on 08.03.2026.
+//
---
+++ Test.h
@@ -0,0 +1,8 @@
+//
+// Created by Lev Dymchenko on 07.03.2026.
+//
+
+#ifndef FSR_PRACTICE_TEST_H
+#define FSR_PRACTICE_TEST_H
+
+#endif //FSR_PRACTICE_TEST_H
