d3f6e09fcabeb1e943b832ef6b1f326dafe9da5f
leo <gd.cats.creators@gmail.com>
2026-03-07 18:47:10 +0300

    sceleton

--- main.c
+++ main.c
@@ -2,7 +2,23 @@
 #include <stdlib.h>
 #include "matrix.h"
 
+Mtype calculateWithGaussMethod(Matrix* m) {
+    for (size_t i = 0; i < m->size_; ++i) {
+        for (size_t j = i + 1; j < m->size_; ++j) {
+        }
+    }
+}
+
+Mtype stupidCalculate(Matrix* m) {
+
+}
 
 int main() {
+    size_t sz;
+    scanf("%llu", &sz);
+    Matrix* m = create(sz);
+    Matrix* cp = copy(m);
 
+    free(destroy(cp));
+    free(destroy(m));
 }
