46dab53a534a6ad0f04b0a87f528bc4061b2f59f
leo <gd.cats.creators@gmail.com>
2026-03-08 17:36:13 +0300

    cosmetic

--- run.py
+++ run.py
@@ -23,18 +23,40 @@ def compile_program():
                    capture_output=False,
                    shell=True)
 
-def get_test_amount() -> int:
+def get_arguments() -> dict:
     arguments = argparse.ArgumentParser()
-    arguments.add_argument("-q", "--quantity",
-                       dest="quantity",
+    arguments.add_argument("-t", "--tests",
+                       dest="tests",
                        type=int,
-                       help="Amount of tests")
-    return int(arguments.parse_args().quantity)
+                       help="Amount of tests(default=10)")
 
-def run_test() -> int:
+    arguments.add_argument("-s", "--size",
+                           dest="size",
+                           type=int,
+                           help="Fixes sizes of matrices(default=-1)")
+
+    arguments.add_argument("-m", "--max",
+                           dest="max",
+                           type=int,
+                           help="Set max size of generating matrices(default=5)")
+
+    args = dict(vars(arguments.parse_args()))
+
+    if args["max"] is None:
+        args["max"] = 5
+    if args["size"] is None:
+        args["size"] = -1
+    if args["tests"] is None:
+        args["tests"] = 10
+
+    return args
+
+def run_test(size=-1, max_sz=-1) -> int:
     path = ["cmake-build-debug/FSR_practice"]
 
-    size = random.randint(1, 5)
+    if size == -1:
+        size = random.randint(1, max_sz)
+
     matrix = [[
         round(random.uniform(0, 15), random.randint(0, 3))
             for _ in range(size)
@@ -75,11 +97,16 @@ def run_test() -> int:
             print(f"Result:\n{res}")
             return 1
         elif res["Gauss_method_result"] != res["Np"]:
-            print("\033[1;31mERROR: GAUSS RESULT NOT EQUAL TO NUMPY\n")
+            print(f"{"\033[1;31mERROR: GAUSS RESULT NOT EQUAL TO NUMPY":<120}", end="")
+            if res["Gauss_method_result"] == res["Minor_method_result"]:
+                print("\033[1;33mWARNING: PROBABLY ROUNDING ERROR\033[0m")
+            print("\033[1;31m", end="")
             print(size)
             print(mat)
             print(f"Result:\n{res}")
-            return 0
+            if res["Gauss_method_result"] == res["Minor_method_result"]:
+                return 2
+            return 1
         output = f"\033[1;32mOK: {res}"
         print(f"{output:<120}PASSED\033[0m")
     else:
@@ -92,13 +119,20 @@ def run_test() -> int:
 
 def main() -> int:
     compile_program()
-    amount = get_test_amount()
+    args = get_arguments()
+    amount = args["tests"]
     passed = 0
+    warnings = 0
+    errors = 0
     for _ in range(amount):
-        passed += 1 - run_test()
+        result = run_test(args["size"], args["max"])
+        passed += result == 0
+        errors += result == 1
+        warnings += result == 2
 
     print(f"\033[1;32m\n\nPassed {passed} tests of {amount}.\033[0m")
-
+    print(f"\033[1;31m{errors} errors.\033[0m")
+    print(f"\033[1;33m{warnings} warnings.\033[0m\n")
     return 0
 
 
